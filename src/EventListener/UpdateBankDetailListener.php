f10cd4014242820f97ab890be6132b78d9dcc7e7
Léo DEDIT <leo@antilop.fr>
2021-10-08 12:19:50 +0200

    BACK-204 - update event listener

--- src/EventListener/UpdateBankDetailListener.php
+++ src/EventListener/UpdateBankDetailListener.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Antilop\SyliusPayzenBundle\EventListener;
 
-use Antilop\SyliusPayzenBundle\Factory\PayzenClientFactory;
 use App\Entity\Shipping\Shipment;
 use App\Entity\Subscription\SubscriptionDraftOrder;
 use App\Entity\Subscription\SubscriptionState;
 use App\Event\SubscriptionEvent;
 use App\Repository\SubscriptionDraftOrderRepository;
 use App\StateMachine\SubscriptionDraftOrderTransitions;
-use Google\Cloud\PubSub\Subscription;
+use App\Entity\Subscription\Subscription;
 use SM\Factory\FactoryInterface as StateMachineFactoryInterface;
 
 class UpdateBankDetailListener
@@ -31,11 +30,15 @@ class UpdateBankDetailListener
         /** @var Subscription $subscription */
         $subscription = $subscriptionEvent->getSubscription();
 
-        /** @var SubscriptionDraftOrder $draftOrder */
-        $draftOrder = $subscription->getDraftOrder();
-        if ($draftOrder->getState() === SubscriptionDraftOrder::STATE_DRAFT_PAYMENT_FAILED) {
-            $stateMachine = $this->stateMachineFactory->get($draftOrder, SubscriptionDraftOrderTransitions::GRAPH);
-            $stateMachine->apply(SubscriptionDraftOrderTransitions::TRANSITION_PROCESS_PAYMENT);
+        $paymentMethod = $subscription->getMethod();
+
+        if ($paymentMethod->getCode() === 'PAYZEN') {
+            /** @var SubscriptionDraftOrder $draftOrder */
+            $draftOrder = $subscription->getDraftOrder();
+            if ($draftOrder->getState() === SubscriptionDraftOrder::STATE_DRAFT_PAYMENT_FAILED) {
+                $stateMachine = $this->stateMachineFactory->get($draftOrder, SubscriptionDraftOrderTransitions::GRAPH);
+                $stateMachine->apply(SubscriptionDraftOrderTransitions::TRANSITION_PROCESS_PAYMENT);
+            }
         }
     }
 }
